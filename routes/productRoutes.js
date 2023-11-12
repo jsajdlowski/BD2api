ac49c0042cf6584516da13c7490a9b0c82693501
Julia Sajdlowski <juliasajdlowski@gmail.com>
2023-11-12 22:23:14 +0100

    add routes

---
+++ routes/productRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const Product = require("../models/product");
+const {
+  getProducts,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+  generateReport,
+} = require("../controllers/productController");
+
+router.route("/").get(getProducts).post(addProduct);
+router.route("/:id").put(updateProduct).delete(deleteProduct);
+router.route("/state").get(generateReport);
+
+module.exports = router;
